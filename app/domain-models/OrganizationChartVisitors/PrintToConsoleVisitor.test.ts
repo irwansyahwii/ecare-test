213239708aec318d391ce9add22b5054aace2512
Irwansyah <irwansyah@gmail.com>
2024-08-02 15:44:17 +0700

    chore: add more test to PrintToConsoleVisitor.test

--- app/domain-models/OrganizationChartVisitors/PrintToConsoleVisitor.test.ts
+++ app/domain-models/OrganizationChartVisitors/PrintToConsoleVisitor.test.ts
@@ -4,17 +4,24 @@ import { Id } from "../Id";
 import { Name } from "../Name";
 import { OrganizationChart } from "../OrganizationChart";
 import data from "../correct-employees.json";
+import forest from "../forest.json";
 import { EmployeeLogger, PrintToConsoleVisitor } from "./PrintToConsoleVisitor";
 
 class ToMemoryLogger implements EmployeeLogger {
   public IndexById:any = {};
   Log(employee: Employee): void {
-    this.IndexById[employee.Id.Value] = employee;
+    const key = employee.Id.Value;
+    if(this.IndexById[key]){
+      throw new Error(`Already visited id: ${key}`);
+    }
+    this.IndexById[key] = employee;
+
+    console.log('id:', employee.Id);
   }
 
 }
 
-test("Load from correct-employees.json, must visit all the nodes", ()=>{
+test("Load from correct-employees.json, must visit all the nodes only once", ()=>{
   const orgStructure = new OrganizationChart();
   data.forEach(d => {
     const employee = new Employee(new Id(d.id + ""), new Name(d.name), d.managerId ? new Id(d.managerId + "") : null, []);
@@ -28,13 +35,39 @@ test("Load from correct-employees.json, must visit all the nodes", ()=>{
   const visitor = new PrintToConsoleVisitor(memoryLogger);
   orgStructure.Accept(visitor);
 
-  data.forEach(d => {
+  for(let i = 0 ; i < data.length; i++){
+    const d = data[i];
     const foundEmployee: Employee = memoryLogger.IndexById[d.id + ""];
     const managerId = foundEmployee.ManagerId ? foundEmployee.ManagerId.Value : null;
     expect(foundEmployee.Id.Value).equals(d.id + "");
     expect(managerId).equals(d.managerId ? d.managerId + "": null);
-  })
+
+  }
+
 
 })
 
+test("Load from forest.json, must visit all the nodes only once", ()=>{
+  const orgStructure = new OrganizationChart();
+  forest.forEach(d => {
+    const employee = new Employee(new Id(d.id + ""), new Name(d.name), d.managerId ? new Id(d.managerId + "") : null, []);
+    orgStructure.Add(employee);
+  });
+
+  orgStructure.IsValid;
+
+  const memoryLogger = new ToMemoryLogger();
+
+  const visitor = new PrintToConsoleVisitor(memoryLogger);
+  orgStructure.Accept(visitor);
+
+  for(let i = 0 ; i < forest.length; i++){
+    const d = forest[i];
+    const foundEmployee: Employee = memoryLogger.IndexById[d.id + ""];
+    const managerId = foundEmployee.ManagerId ? foundEmployee.ManagerId.Value : null;
+    expect(foundEmployee.Id.Value).equals(d.id + "");
+    expect(managerId).equals(d.managerId ? d.managerId + "": null);
+  }
+
+})
 
