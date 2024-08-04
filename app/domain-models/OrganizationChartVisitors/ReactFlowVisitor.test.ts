00cf3eb490e82f669405c8559028e52152f06cca
Irwansyah <irwansyah@gmail.com>
2024-08-04 12:57:42 +0700

    fix: ReactFlowVisitor.test to accomodate duplicate employee names

--- app/domain-models/OrganizationChartVisitors/ReactFlowVisitor.test.ts
+++ app/domain-models/OrganizationChartVisitors/ReactFlowVisitor.test.ts
@@ -19,88 +19,121 @@ test("Output the correct nodes, edges, direct report and indirect reports counts
   orgStructure.Accept(visitor);
 
   const expectedNodes = [
-    { id: '1', data: { label: 'raelynn (2/9)' }, position: { x: 0, y: 0 } },
-    { id: '2', data: { label: 'darin (3/3)' }, position: { x: 0, y: 0 } },
-    { id: '4', data: { label: 'jordana (0/0)' }, position: { x: 0, y: 0 } },
-    { id: '5', data: { label: 'everett (0/0)' }, position: { x: 0, y: 0 } },
-    { id: '6', data: { label: 'bertha (0/0)' }, position: { x: 0, y: 0 } },
-    { id: '3', data: { label: 'kacie (3/4)' }, position: { x: 0, y: 0 } },
-    { id: '7', data: { label: 'peg (0/0)' }, position: { x: 0, y: 0 } },
-    { id: '8', data: { label: 'hugh (0/0)' }, position: { x: 0, y: 0 } },
-    { id: '9', data: { label: 'eveleen (1/1)' }, position: { x: 0, y: 0 } },
-    {
-      id: '10',
-      data: { label: 'evelina (0/0)' },
-      position: { x: 0, y: 0 }
-    }
-  ]
+  {
+    id: '1',
+    data: { label: 'raelynn (3/10)' },
+    position: { x: 0, y: 0 }
+  },
+  { id: '2', data: { label: 'darin (3/3)' }, position: { x: 0, y: 0 } },
+  {
+    id: '4',
+    data: { label: 'jordana (0/0)' },
+    position: { x: 0, y: 0 }
+  },
+  {
+    id: '5',
+    data: { label: 'everett (0/0)' },
+    position: { x: 0, y: 0 }
+  },
+  {
+    id: '6',
+    data: { label: 'bertha (0/0)' },
+    position: { x: 0, y: 0 }
+  },
+  { id: '3', data: { label: 'kacie (3/4)' }, position: { x: 0, y: 0 } },
+  { id: '7', data: { label: 'peg (0/0)' }, position: { x: 0, y: 0 } },
+  { id: '8', data: { label: 'hugh (0/0)' }, position: { x: 0, y: 0 } },
+  {
+    id: '9',
+    data: { label: 'eveleen (1/1)' },
+    position: { x: 0, y: 0 }
+  },
+  {
+    id: '10',
+    data: { label: 'evelina (0/0)' },
+    position: { x: 0, y: 0 }
+  },
+  {
+    id: '41',
+    data: { label: 'jordana (0/0)' },
+    position: { x: 0, y: 0 }
+  }
+]
 
 const expectedEdges = [
-    {
-      id: 'e2',
-      source: '1',
-      target: '2',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e4',
-      source: '2',
-      target: '4',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e5',
-      source: '2',
-      target: '5',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e6',
-      source: '2',
-      target: '6',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e3',
-      source: '1',
-      target: '3',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e7',
-      source: '3',
-      target: '7',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e8',
-      source: '3',
-      target: '8',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e9',
-      source: '3',
-      target: '9',
-      type: 'smoothstep',
-      animated: true
-    },
-    {
-      id: 'e10',
-      source: '9',
-      target: '10',
-      type: 'smoothstep',
-      animated: true
-    }
-  ]
+  {
+    id: 'e2',
+    source: '1',
+    target: '2',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e4',
+    source: '2',
+    target: '4',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e5',
+    source: '2',
+    target: '5',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e6',
+    source: '2',
+    target: '6',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e3',
+    source: '1',
+    target: '3',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e7',
+    source: '3',
+    target: '7',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e8',
+    source: '3',
+    target: '8',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e9',
+    source: '3',
+    target: '9',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e10',
+    source: '9',
+    target: '10',
+    type: 'smoothstep',
+    animated: true
+  },
+  {
+    id: 'e41',
+    source: '1',
+    target: '41',
+    type: 'smoothstep',
+    animated: true
+  }
+]
 
+  console.log(visitor.initialEdges)
   expect(visitor.initialNodes).toEqual(expectedNodes)
   expect(visitor.initialEdges).toEqual(expectedEdges)
 
